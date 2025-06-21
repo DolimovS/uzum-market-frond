276c18cf85481c9b5210d80345082bc81b6ac17f
DolimovS <samandardolimov71@gmail.com>
2025-06-21 15:39:17 +0500

    Zargarlik Buyumlari

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header'
 
 import './App.css'
-import Slider from './components/Slider/Slider'
 import Elektronika from './components/Elektronika/Elektronika'
 import OneProduct from './OneProduct/OneProduct'
 import Krasovka from './components/Krasovka/Krasovka'
 import Zargarlik from './components/Zargarlik/Zargarlik'
+import UyRozgorBuyumlari from './components/UyRozgorBuyimlari/UyRozgorBuyimlari'
+import Home from './components/Home/Home'
+import SavatProduct from './SavatProduct/SavatProduct'
 
 function App() {
 
@@ -14,10 +16,13 @@ function App() {
     <div className="container">
       <Header/>
         <Routes>
+          <Route path='home' element={<Home/>} />
           <Route path='elektronika' element={<Elektronika/>} />
           <Route path='/krasovka' element={<Krasovka/>} />
           <Route path='/zargarlikBuyumlari' element={<Zargarlik/>} />
-          <Route path='slider' element={<Slider/>}/>
+          <Route path='/uyRozgorBuyumlari' element={<UyRozgorBuyumlari/>} />
+          <Route path='/savat' element={<SavatProduct/>} />
+          
           <Route path='/oneProduct/:id' element={<OneProduct/>}/>
         </Routes>
     </div>
